000b64efeaf414afa633f5dc5e52bd46e4e283cd
anirban2005143a <23je0104@iitism.ac.in>
2024-08-03 01:19:22 +0530

    creating a section for home page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Achievements from "./components/landing page/achievements"
+
+function App() {
+
+
+  return (
+    <>
+      <Achievements />
+    </>
+  )
+}
+
+export default App
